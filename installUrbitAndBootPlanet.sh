417e46aca05df4ae9363adc9eaab6df4bef6b2a6
Ben Calder <263295+bencalder@users.noreply.github.com>
2018-06-23 01:06:58 -0500

    Escaped the parentheses in prompt instructions

--- installUrbitAndBootPlanet.sh
+++ installUrbitAndBootPlanet.sh
@@ -3,9 +3,9 @@
 
 
 # get planet and ticket
-echo Enter your planet name (include the tilde and hyphen, e.g. ~mirfet-hocbyt)
+echo Enter your planet name \(include the tilde and hyphen, e.g. ~mirfet-hocbyt\)
 read planetname
-echo Enter your ticket (include the tilde and hyphens here too)
+echo Enter your ticket \(include the tilde and hyphens here too\)
 read ticket
 
 
