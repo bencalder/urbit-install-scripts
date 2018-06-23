b8281ca6b52c8736dc563489515fa23b04138135
Ben Calder <263295+bencalder@users.noreply.github.com>
2018-06-22 19:53:47 -0500

    Add boot script

---
+++ installAndBootUrbit.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+# install and boot urbit
+
+
+echo Enter your planet name
+read planetname
+echo Enter your ticket
+read ticket
+
+
+# swap file
+sudo fallocate -l 2G /swapfile
+
+ls -lh /swapfile
+
+sudo chmod 600 /swapfile
+
+ls -lh /swapfile
+
+sudo mkswap /swapfile
+
+sudo swapon /swapfile
+
+sudo cp /etc/fstab /etc/fstab.bak
+
+
+# download source and dependencies then install
+sudo apt-get update
+
+yes Y | sudo apt-get install autoconf automake cmake exuberant-ctags g++ git libcurl4-gnutls-dev libgmp3-dev libncurses5-dev libsigsegv-dev libssl-dev libtool make ninja-build openssl pkg-config python python3 python3-pip ragel re2c zlib1g-dev
+
+sudo -H pip3 install --upgrade pip
+
+sudo -H pip3 install meson
+
+git clone https://github.com/urbit/urbit
+
+cd urbit/
+
+./scripts/bootstrap
+
+./scripts/build
+
+sudo ninja -C ./build/ install
+
+urbit
+
+urbit -w $planetname -t $ticket
